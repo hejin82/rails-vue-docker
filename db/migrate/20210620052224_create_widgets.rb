955eb03243947133e00660330368a1aedc8f9165
hejin <hejin82@gmail.com>
2021-06-20 14:31:07 +0900

    rails db:migrate

--- db/migrate/20210620052224_create_widgets.rb
+++ db/migrate/20210620052224_create_widgets.rb
@@ -2,7 +2,7 @@ class CreateWidgets < ActiveRecord::Migration[6.1]
   def change
     create_table :widgets do |t|
       t.string :name
-      t.int :quantity
+      t.integer :quantity
       t.text :description
 
       t.timestamps
