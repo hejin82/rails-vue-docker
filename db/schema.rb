f7f108ef0329ea323885ee2af9001c2b9f539d1a
hejin <hejin82@gmail.com>
2021-06-21 19:36:06 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_20_063927) do
+ActiveRecord::Schema.define(version: 2021_06_20_071917) do
+
+  create_table "articles", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "published_"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "manufacturers", charset: "utf8mb4", force: :cascade do |t|
     t.text "name"
